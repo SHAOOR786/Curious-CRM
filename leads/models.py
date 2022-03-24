f864105fec1ad36fecb4c2ce2337f000f603fc64
Shaoor Ud Din Hashmi <aquariusca01@gmail.com>
2022-03-24 18:36:38 -0400

    new model added agent

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,11 @@
+from statistics import mode
+from tkinter import CASCADE
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    pass
 
 class Lead(models.Model):
     first_name = models.CharField(
@@ -12,4 +17,9 @@ class Lead(models.Model):
     age = models.IntegerField(
         default=0
     )
-    
+    agent=models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+  
